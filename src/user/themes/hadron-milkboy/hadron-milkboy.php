232f9fabf3e8a6ca8b4c4a6ac94aa7b79be44068
milkboy007 <milk_boy007@hotmail.com>
2026-07-22 03:18:43 +0700

    Create hadron-milkboy sub-theme with theme inheritance and restore base hadron theme

---
+++ src/user/themes/hadron-milkboy/hadron-milkboy.php
@@ -0,0 +1,9 @@
+<?php
+namespace Grav\Theme;
+
+use Grav\Common\Theme;
+
+class HadronMilkboy extends Theme
+{
+    // Custom sub-theme logic inheriting all streams and templates from Hadron base theme
+}
