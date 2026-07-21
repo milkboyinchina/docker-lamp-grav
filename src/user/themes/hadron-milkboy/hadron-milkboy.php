c4482b4ced611f7a0f82285cbc51a7ff7f4c09ad
milkboy007 <milk_boy007@hotmail.com>
2026-07-22 03:22:09 +0700

    Fix stream type to ReadOnlyStream, register twig namespaces in HadronMilkboy PHP class, and fix custom.ini comment syntax

--- src/user/themes/hadron-milkboy/hadron-milkboy.php
+++ src/user/themes/hadron-milkboy/hadron-milkboy.php
@@ -1,9 +1,34 @@
 <?php
 namespace Grav\Theme;
 
-use Grav\Common\Theme;
+use Grav\Common\Grav;
 
-class HadronMilkboy extends Theme
+// Include Hadron base theme PHP class
+$hadronPath = __DIR__ . '/../hadron/hadron.php';
+if (file_exists($hadronPath)) {
+    require_once $hadronPath;
+}
+
+class HadronMilkboy extends Hadron
 {
-    // Custom sub-theme logic inheriting all streams and templates from Hadron base theme
+    public static function getSubscribedEvents()
+    {
+        return array_merge(parent::getSubscribedEvents(), [
+            'onTwigLoader' => ['onTwigLoader', 0],
+        ]);
+    }
+
+    public function onTwigLoader()
+    {
+        parent::onTwigLoader();
+
+        // Add parent theme namespaces to Twig
+        $locator = Grav::instance()['locator'];
+        foreach (['hadron', 'quark'] as $themeName) {
+            $path = $locator->findResource('themes://' . $themeName);
+            if ($path) {
+                $this->grav['twig']->addPath($path . DIRECTORY_SEPARATOR . 'templates', $themeName);
+            }
+        }
+    }
 }
